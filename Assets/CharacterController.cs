433042a9ba2e6f01d7ca07dd8bdab7a53575a737
Safwoen <113114216+Safwoen@users.noreply.github.com>
2023-01-12 15:35:14 +0000

    character controller almost done

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 1.0f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
+    public float rotationSpeed = 2.0f;
+    public float camRotationSpeed = 1.5f;
+
+    //public Animator myAnim;
+
+    void Start()
+    {
+
+
+        //myAnim = GetComponentInChildren<Animator>();
+        cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        //myAnim.SetBool("isOnGround", isOnGround);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+
+        {
+            //myAnim.SetTrigger("jumped");
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+
+        //myAnim.SetFloat("speed", newVelocity.magnitude);
+
+
+        rotation = rotation + Input.GetAxis("Mouse X");
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        //camRotation = camRotation - Input.GetAxis("Mouse Y");
+        //cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+
+        //camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+    }
+}
